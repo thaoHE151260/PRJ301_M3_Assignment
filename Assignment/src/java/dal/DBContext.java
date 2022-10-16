105bad28bb199996fb37bfc60c76a1e9e8ee0c3f
thaoHE151260 <97202947+thaozz23112001@users.noreply.github.com>
2022-10-16 08:53:55 +0700

    create database context
    create database context

---
+++ Assignment/src/java/dal/DBContext.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dal;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author DELL i7
+ */
+public abstract class DBContext<T> {
+    protected Connection connection;
+    public DBContext()
+    {
+        try {
+            String user = "thaont";
+            String pass = "123456789";
+            String url = "jdbc:sqlserver://localhost\\MSSQLSERVER01:1433;databaseName=PRJ301_Slot3_Day2,4,6";
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+            connection = DriverManager.getConnection(url, user, pass);
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(DBContext.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (SQLException ex) {
+            Logger.getLogger(DBContext.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+}
